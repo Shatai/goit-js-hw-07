434d28cf04bfc62b9526778fd359c9c63e1e2918
Denys Scherbaniyk <denysand0504@gmail.com>
2022-08-29 04:33:41 +0300

    main

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,57 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+// import * as basicLightbox from 'basiclightbox'
+// const instance = basicLightbox.create();
+// instance.show();
+const divEl = document.querySelector(".gallery");
+
+function createCards (galleryItems){
+
+    return galleryItems.map((item) => {
+
+        return `<div class="picture"><a href="${item.original}"><img src="${item.preview}" data-source="${item.original}"
+        alt="${item.description}"/></a></div>`
+  
+    }).join("");
+};
+const markup = createCards(galleryItems);
+divEl.insertAdjacentHTML("beforeend", markup);
+
+
+divEl.addEventListener("click", onClick);
+
+function onClick(event) {
+    event.preventDefault();
+    if(event.target.nodeName !== 'IMG'){
+        return;
+    }
+    openLightBox(event);
+
+    
+    document.addEventListener("keydown", event => {
+        if(event.code === "Escape") {
+        lightBox.close();
+        }
+    
+
+    });
+  
+};
+
+let lightBox;
+function openLightBox(event) {
+    lightBox = basicLightbox.create(`<img src="${event.target.dataset.source}" />`);
+    lightBox.show();
+
+};
+
+
+
+
+
+
+
+
+
+
