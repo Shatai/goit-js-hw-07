dcf9fce3301d97d8926e78137b186d8596d9f309
Denys Scherbaniyk <denysand0504@gmail.com>
2022-08-30 21:33:47 +0300

    main

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,48 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+// import SimpleLightbox from "simplelightbox";
 
 console.log(galleryItems);
+
+const divEl = document.querySelector(".gallery");
+
+
+divEl.addEventListener("click", onClick);
+
+
+function createCards (galleryItems){
+
+    return galleryItems.map((item) => {
+
+        return `<li><a href="${item.original}"><img src="${item.preview}" data-source="${item.original}"
+        alt="${item.description}"/></a></li>`
+  
+    }).join("");
+};
+const markup = createCards(galleryItems);
+divEl.insertAdjacentHTML("beforeend", markup);
+
+
+function onClick(event) {
+    event.preventDefault();
+    if(event.target.nodeName !== 'IMG'){
+        return;
+    }
+    openModalSimpleLightbox(event);
+
+  
+};
+
+function openModalSimpleLightbox(event){
+    let simpleLightbox = new SimpleLightbox('.gallery a',
+        {
+            captionSelector: 'img',
+            captionType: 'attr',
+            captionsData: 'alt',
+            animationSpeed: 150,
+            captionPosition: 'bottom',
+            captionDelay: 250,
+            enableKeyboard: true,
+        });
+        simpleLightbox.open();
+};
